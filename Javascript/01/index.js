65433a7c824b3dafae77b1f582c903bc0b209147
Cristiane11 <bjwmcd@yahoo.com>
2020-10-21 18:20:57 -0400

    update

--- Javascript/01/index.js
+++ Javascript/01/index.js
@@ -1,10 +1,22 @@
 let userName = 'Cristiane Abreu';
 let message = `Hi ${username}, How are you?`;
   
-console.log( message);
+//console.log( message);
 // template literal ``
 
-const weight = 150;
-const moonWeight = `You weight ${weight * 0.165} pounds on the moon`;
-console.log(moonWeight);
+//const weight = 150;
+//const moonWeight = `You weight ${weight * 0.165} pounds on the moon`;
+//console.log(moonWeight);
+
+//const threeLines = "This is a string \n that spans across \r three lines.";
+
+const threeLines = `This is a string 
+that spans across 
+three lines.
+`;
+// Challenge: Help Darth Vader express himself to his son!
+const mySon = "Luke";
+const parentalStatus = "father";
+const message = `${mySon}, I am your ${parentalStatus}`;
+console.log(message);
 
