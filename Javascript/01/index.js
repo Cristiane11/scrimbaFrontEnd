93c8d2bcbf2181e06257689958b5591d31910cd4
Cristiane11 <bjwmcd@yahoo.com>
2020-10-21 08:22:26 -0400

    update

--- Javascript/01/index.js
+++ Javascript/01/index.js
@@ -2,4 +2,9 @@ let userName = 'Cristiane Abreu';
 let message = `Hi ${username}, How are you?`;
   
 console.log( message);
+// template literal ``
+
+const weight = 150;
+const moonWeight = `You weight ${weight * 0.165} pounds on the moon`;
+console.log(moonWeight);
 
