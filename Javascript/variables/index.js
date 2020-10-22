1824fcc818a171baa504ce20f01b87fae4f094c5
Cristiane11 <bjwmcd@yahoo.com>
2020-10-22 06:40:06 -0400

    update

---
+++ Javascript/variables/index.js
@@ -0,0 +1,47 @@
+let userName = 'Cristiane Abreu';
+let message = `Hi ${username}, How are you?`;
+  
+//console.log( message);
+// template literal ``
+
+//const weight = 150;
+//const moonWeight = `You weight ${weight * 0.165} pounds on the moon`;
+//console.log(moonWeight);
+
+//const threeLines = "This is a string \n that spans across \r three lines.";
+
+const threeLines = `This is a string 
+that spans across 
+three lines.
+`;
+// Challenge: Help Darth Vader express himself to his son!
+//const mySon = "Luke";
+//const parentalStatus = "father";
+//const message = `${mySon}, I am your ${parentalStatus}`;
+//console.log(message);
+// const name = 'Jane';
+// const Name = 'Mark';
+// const NAME = 'Fred';
+
+// console.log(name, Name, NAME);
+
+// let fullName = `${firstName} ${lastName}`;
+
+let firstname;
+let first_name;
+let FIRSTNAME;
+let FIRST_NAME;
+let firstName;
+
+// is, has
+// let isModalVisible = true;
+
+// if (isModalVisible) {
+//   // do something  
+// }
+
+// let isLoading;
+// let hasPosition;*
+
+//variable with all cap it should for value that never changes
+const COLOR_RED = '#f00';
