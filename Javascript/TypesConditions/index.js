8acf6a81123cad06d61379b2de221ea33f9738f8
Cristiane11 <bjwmcd@yahoo.com>
2020-10-24 06:59:45 -0400

    update[

---
+++ Javascript/TypesConditions/index.js
@@ -0,0 +1,13 @@
+// if statements- evaluate boolean values
+const preferDarkMode = false;
+const preferSolarizedMode = false;
+
+if(preferDarkMode ){
+    console.log('dark mode set!');
+    document.body.style.background = 'black';
+}else if (){
+
+}else{
+    console.log('light mode set!');
+    document.body.style.background = 'ghostwhite';
+}
