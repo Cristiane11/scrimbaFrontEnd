1880f7b4009293f32fc771a8bd1b2543d56e1808
Cristiane11 <bjwmcd@yahoo.com>
2020-11-03 06:39:57 -0500

    update

---
+++ Javascript/typesOfData.js
@@ -0,0 +1,23 @@
+/*Primitive types:
+
+string
+number
+boolean
+undefined
+null
+symbol
+
+everything else - Object type
+*/
+let message = 'some string';
+
+console.log(typeof message);
+
+// 1) Explicit type conversion
+
+
+/* 2) Implicit type conversion (coercion)
+this example is when the JS convert the string to a number without we telling js
+*/
+
+console.log('1' * '2');
